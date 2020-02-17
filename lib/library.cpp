a8b122f4805a0a621aa7be460dcd119e7e8c9538
Vincent Guénin <guenin.vincent4@gmail.com>
2020-02-17 12:55:18 +0100

    First commit

---
+++ lib/library.cpp
@@ -0,0 +1,12 @@
+#include "library.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
+
+int main(int argc, char **argv) {
+    hello();
+    return 0;
+}
